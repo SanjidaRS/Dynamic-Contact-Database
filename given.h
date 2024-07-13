02fac964760f297d335e5aa70ff3d52d35d8b2c1
Sanjida Rahman Supti <166355719+SanjidaRS@users.noreply.github.com>
2024-07-13 15:47:25 +0600

    given.h

---
+++ given.h
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct contact {
+   long empIdPosn;
+   long firstNamePosn;
+   long lastNamePosn;
+   long emailPosn;
+   long next;
+};
+
+void createContacts (char * fileName);
+
+void readContacts (char * fileName);
+
+int searchContacts (char * fileName, int keyId);
+

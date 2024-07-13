0163b7dc682f8e1d3ffcf5d92c8c58c81f40b93e
Sanjida Rahman Supti <166355719+SanjidaRS@users.noreply.github.com>
2024-07-13 15:45:56 +0600

    contacts.c

--- contacts.c
+++ contacts.c
@@ -1,20 +1,4 @@
-/*
-Student Name: Sanjida Rahman Supti
-Student ID: 1215651
-Due Date: 10th March 2024
-Course: CIS*2500
-I have exclusive control over this submission via my password.
-By including this header comment, I certify that:
-1) I have read and understood the policy on academic integrity.
-2) I have completed Moodle's module on academic integrity.
-3) I have achieved at least 80% on the academic integrity quiz
-I assert that this work is my own. I have appropriate acknowledged
-any and all material that I have used, be it directly quoted or
-paraphrased. Furthermore, I certify that this assignment was written
-by me in its entirety.
-*/
-
-#include "givenA2.h"
+#include "given.h"
 
 int main ()
 {
